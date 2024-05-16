f5d95d6ae90f913c1b13b733f3dce987af24e38e
James Esguerra <jesguerra@stealthmonitoring.com>
2024-05-16 20:42:44 +0800

    initial react setup

---
+++ src/components/Sample.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sample = () => {
+  return (
+    <div>
+        <button onClick={console.log("hello world")}>hello</button>
+    </div>
+  )
+}
+
+export default Sample

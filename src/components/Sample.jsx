abf3d874991c900ea7970e0720ff62a43c16967e
James Esguerra <jesguerra@stealthmonitoring.com>
2024-05-16 21:28:25 +0800

    framer motion & react test

--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import "../styles/styles.css";
+import { motion } from "framer-motion";
 
 const Sample = () => {
   return (
-    <div>
-        <button onClick={console.log("hello world")}>hello</button>
+    <div className="example-container">
+      <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} />
     </div>
   )
 }
---
+++ src/scripts/main.js
@@ -0,0 +1,2 @@
+var twenty = document.querySelector(".gradient-text.text-center.font-extrabold");
+twenty.style.color = "red";

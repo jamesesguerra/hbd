82c899dcf21bf25e47c81b71d64b7851ec4c3442
James Esguerra <jesguerra@stealthmonitoring.com>
2024-06-28 23:04:20 +0800

    fix url

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 import type { DateRequest } from "~/types";
 
-const BASE_URL = "http://localhost:3001/email";
-
-// https://movie-app-server-production-42c3.up.railway.app/
-
+const BASE_URL = "https://movie-app-server-production-42c3.up.railway.app/";
 
 const send = (date: DateRequest) => {
     const request = axios.post(BASE_URL, date);

9c768ae1a558d9a34d0b61bba3c51083b1ff4a1b
James Esguerra <jamesesguerra025@gmail.com>
2025-06-26 12:55:36 +0800

    add pokemon event pix

--- src/data/2025/messages.ts
+++ src/data/2025/messages.ts
@@ -1,16 +1,14 @@
 const messages: any[] = [
     {
         imageUrl: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg',
-        emotion: 'ure feeling nostalgic',
-        message: `
-from our first few times talking palang, i could already tell how smart you are. to me the position you're in right now is cos of unfortunate events but it doesn't mean you're not smart / not good at anything. the thought that you're not smart enough to pursue med / careers you really want never even crossed my mind cos i've thought really highly about you from the start palang. there's nothing i'd wish to change about you and if there's anything i could wish it's for you to see yourself through my eyes. i'm really soso proud you're my gf to the point where i'd wanna see my parents / relatives just to show off how lucky i am with you. i just always want you to remember that there's always someone who believes in you more than anything & anyone
-`,
+        emotion: 'ure feeling alone',
+        message: `hi baby, sorry that you're feeling alone. there are things talaga na no one will really quite get, including me. just know na i really try my best to understand how you're feeling, its just there are times lang din na i have to step into your shoes to get you. despite that, i still see and care about you, both the strong and weak parts. i hope you never feel the need to pretend with me because i want to understand you, and how u think, and why certain situations make you feel certain emotions. i wanna walk with you every step of the way and i want you to feel that no matter what happens i'll always be here holding your hand through everything, and i'll always be the person who sides with you even when it seems like the whole world is against you`,
         pokemon: 'cubone'
     },
     {
         imageUrl: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg',
         emotion: 'u wanna know how happy u make me',
-        message: 'Another hidden text',
+        message: 'hi babby, i wish you could understand how happy you make me. i know i always say it but it\'s the kind of happiness that makes me feel like a kid again. you help me realize that even though it’s scary knowing we have only so much time in this world, we should still live in a way that chases love and wonder and curiosity, and not forget that the richest moments in life don\'t come from getting a promotion / raise. that after a long day of work and being stressed and worrying about countless things, i always have you to ground me, to remind me what really matters. ever since that day i finished reading a far wilder magic, i always knew you were the only person / thing that could get me back to being that 11 yr old boy who just wanted to play minecraft all day. i guess thats also why i associate so many minecraft things with you, cos you\'re my reminder that it\'s okay for even the most grown up of people to become kids again. and the happiness that brings me is something i\'d never trade for anything in the world',
         pokemon: 'kyogre2'
     },
     {
